91821d11bd116536e91b80f1f30a1c0ac7d65656
jennysun1001 <sunchenny17@kit.edu.kh>
2018-06-25 20:46:23 +0700

    Not yet Complete

---
+++ List.scala
@@ -0,0 +1,10 @@
+object List
+{
+  def main(args: Array[String]
+  {
+    var FruitsList new ListBuffer[String]()
+    FruitsList+="Mellon"
+    var(l,s) = Fruits.splitAt(3)
+    var Fruits = List("Banana", "Apple", "Strawberry")
+    var t = ("Kiwi", "Orange")
+
