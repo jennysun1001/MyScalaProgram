92f58c1337b2bfe2cc0c72004ddd57dc196bde0c
jennysun1001 <sunchenny17@kit.edu.kh>
2018-06-25 20:12:11 +0700

    MarkForGrade

---
+++ MarkForGrade.scala
@@ -0,0 +1,14 @@
+var num:Int=85
+if(num>=0 && num<50)
+{
+    println("fail")
+}
+else if(num>= 50 && num<60)
+{
+    println("D Grade")
+}
+else if(num>= 60 && num<70)
+{
+    println("C Grade")
+}
+else 
